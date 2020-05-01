65ad5deecdfe9c090050b309a6cee1b7aabcb3bb
Brecht Savelkoul <brecht.savelkoul@alumni.lse.ac.uk>
2020-05-01 21:45:52 +0200

    Derive sender hostname from sender address

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ module.exports.sendMail = sendMail
 
 function sendMail (port, data) {
   var mail = Object.assign({
-    from: 'Someone <some@example.com>',
-    to: 'Another <other@example.com>',
+    from: 'some@example.com',
+    to: 'other@example.com',
     subject: 'Literally anything',
     text: '...'
   }, data)
 
+  var at = mail.from.indexOf('@')
+  var domain = mail.from.substring(at + 1)
+
   return nodemailer.createTransport({
+    name: domain,
     host: '127.0.0.1',
     port: port,
     secure: false,
